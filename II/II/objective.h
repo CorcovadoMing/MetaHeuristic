b6daac33370d6ee8166fc619cb4c40b4a41a8f09
CorcovadoMing <rf37535@gmail.com>
2015-03-19 00:43:39 +0800

    Replace fmax with max

--- II/II/objective.h
+++ II/II/objective.h
@@ -8,7 +8,8 @@ const int evaluate(const Solution& solution, const Matrix& matrix){
 	std::vector<int> timespan(matrix.size() + 1, 0);
 	for (std::size_t i = 0; i < matrix[0].size(); i++){
 		for (std::size_t j = 1; j <= matrix.size(); j++){
-			timespan[j] = std::fmax(timespan[j - 1], timespan[j]) + matrix[j - 1][solution[i]];
+			timespan[j] = std::max(timespan[j - 1], timespan[j]) + matrix[j - 
+1][solution[i]];
 		}
 	}
 	return timespan[matrix.size()];
