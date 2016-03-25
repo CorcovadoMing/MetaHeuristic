ff8888cb27bc71a32e70afaeb6fdd4c4ee02b582
CorcovadoMing <rf37535@gmail.com>
2016-03-25 09:21:08 +0800

    C++11 vector initialization

--- src/GA/main.cpp
+++ src/GA/main.cpp
@@ -8,11 +8,7 @@ const double myfunc(const std::vector<double> &param) {
 
 int main(int argc, const char * argv[]) {
     GeneticAlgorithm geneticAlgorithm(0.6, 20);
-    std::vector<double> rangeMin, rangeMax;
-    rangeMin.push_back(0);
-    rangeMin.push_back(100);
-    rangeMax.push_back(10);
-    rangeMax.push_back(110);
+    std::vector<double> rangeMin {0, 100}, rangeMax {10, 110};
     geneticAlgorithm.initial(2, rangeMin, rangeMax, myfunc);
     do {
         geneticAlgorithm.selection();
