e27a914acdb7b80c1baf7c371e1131cd79c551c9
CorcovadoMing <rf37535@gmail.com>
2016-06-03 06:59:33 +0800

    Roughly done pso init

--- pso_main.cpp
+++ pso_main.cpp
@@ -7,40 +7,28 @@ const double myfunc(const std::vector<double> &param) {
 }
 
 int main(int argc, const char * argv[]) {
-    // Declare GA class with mutation rate 0.6 and population size 20
-    GeneticAlgorithm geneticAlgorithm;
+    ParticleSwarmOptimizer pso;
     
     // Each parameters rangeMin and rangeMax
     std::vector<double> rangeMin {0, 0, 0, 100}, rangeMax {5, 10, 10, 110};
     
     // Parameters
     Parameters param;
-    param.MutationType = Distribution::Normal; // (Distribution::Normal, Distribution::Uniform)
-    param.MutationNormalStd = 0.1;
     param.RangeMax = rangeMax;
     param.RangeMin = rangeMin;
+    param.FitnessFunction = myfunc;
     param.Dimensions = 4;
     param.DimensionsInt = 2;
-    param.FitnessFunction = myfunc;
-    param.Populations = 20;
-    param.MutationRate = 0.6;
     
-    geneticAlgorithm.initial(param);
-    
-    // No need to modify this while loop
-    do {
-        geneticAlgorithm.selection();
-        geneticAlgorithm.crossover();
-        geneticAlgorithm.mutation();
-    } while (geneticAlgorithm.notConverge());
-    //geneticAlgorithm.print(); // for Debugging
-
+    pso.initial(param);
+    pso.print();
+/*    
     // Result is fit into a vector
-    std::vector<double> result = geneticAlgorithm.getResult();
+    std::vector<double> result = pso.getResult();
     for (const auto i : result) {
         std::cout << i << " ";
     }
-    std::cout << std::endl << "Fitness: " << geneticAlgorithm.getFitness() << std::endl;
-
+    std::cout << std::endl << "Fitness: " << pso.getFitness() << std::endl;
+*/
     return 0;
 }
--- src/pso/ParticleSwarmOptimizer.h
+++ src/pso/ParticleSwarmOptimizer.h
@@ -6,53 +6,38 @@
 #include "RandomRange.h"
 #include "Solution.h"
 
-namespace Distribution {
-    extern const int Uniform;
-    extern const int Normal;
-}
-
 class Parameters {
 public:
     int Populations = 20;
-    double MutationRate = 0.6;
     int Dimensions = 0;
     int DimensionsInt = 0;
     std::vector<double> RangeMin, RangeMax;
     const double (*FitnessFunction) (const std::vector<double> &);
-    int MutationType = 0;
-    double MutationNormalStd = 0.2;
+    double w = 0.6;
+    double c1 = 0.7;
+    double c2 = 0.7;
 };
 
-class GeneticAlgorithm {
+class ParticleSwarmOptimizer {
 public:
-    GeneticAlgorithm() {}
+    ParticleSwarmOptimizer() {}
     const std::vector<double> getResult() const;
     const double getFitness() const;
     void print() const;
     void initial(const Parameters &);
-    void selection();
-    void crossover();
-    void mutation();
-    const bool notConverge();
 
 private:
     void evaluate_();
     const double fitnessFunction_(const Solution &) const;
     const double (*fitnessFunc_) (const std::vector<double> &);
-    int mutation_type_;
-    double mutation_std_;
     int population_;
     int int_index_;
     double best_fitness_ = INT32_MIN;
-    double last_best_fitness_ = INT32_MIN;
-    const int converge_ = 3;
-    int still_ = converge_;
     Solution best_solution_;
-    double mutation_rate_;
-    std::vector<Solution> solutions_;
-    std::vector<double> fitness_;
-    std::vector<int> selected_;
+    std::vector<Solution> positions_, pbest_positions_;
+    std::vector<double> fitness_, velocity_, gbest_position_;
     std::vector<double> rangeMin_, rangeMax_;
+    double w_, c1_, c2_;
 };
 
 #endif
